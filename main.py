70c2f0f1c0d2299fac60e7fa4de454d003a86349
Praabindh Pradeep <63050949+praabindhp@users.noreply.github.com>
2021-03-08 20:24:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,15 @@ from bs4 import BeautifulSoup as soup
 my_url      = "https://subslikescript.com/series/Two_and_a_Half_Men-369179/season-1/episode-1-Pilot"
 
 u_client    = ureq(my_url)
+
 page_html   = u_client.read()
+
 u_client.close()
 
 page_soup   = soup(page_html, "html.parser")
+
 script      =  page_soup.find("div", {"class":"full-script"})
 
 outfile     = open('./script.txt', 'w')
+
 outfile.write(script.text)
